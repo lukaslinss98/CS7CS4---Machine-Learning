e95957c0b8633eb94b12365c58eba732ceb722bc
Lukas Linss <lukas.linss@gmail.com>
2026-01-01 20:29:52 +0000

    minor fix

--- final/boolean/boolean_gpt.py
+++ final/boolean/boolean_gpt.py
@@ -61,7 +61,7 @@ class Head(nn.Module):
         super().__init__()
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, kead_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
